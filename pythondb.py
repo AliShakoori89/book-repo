5a93dbd456db366dcbf4599a9c9ef425914f1d5c
Ali Shakoori <alishakoori89@gmail.com>
2019-09-18 15:54:22 +0430

    edit code

--- pythondb.py
+++ pythondb.py
@@ -16,13 +16,13 @@ class Sqlite3_db:
         self.author=self.conn3.cursor()
 
     def create_table(self):
-        self.publisher.execute('CREATE TABLE IF NOT EXISTS Publisher(Name TEXT(50),Address TEXT(200),Phone INTEGER,URL TEXT(50))')
+        self.publisher.execute('CREATE TABLE IF NOT EXISTS Publisher(PName TEXT(50),Address TEXT(200),Phone INTEGER,URL TEXT(50))')
         self.conn1.commit()
 
-        self.book.execute('CREATE TABLE IF NOT EXISTS book(ISBN VARCHAR(255),Publisher_Name TEXT(50),Author_Name TEXT(200),Author_Address TEXT(100),year INTEGER,Title TEXT(50),Price INTEGER)')
+        self.book.execute('CREATE TABLE IF NOT EXISTS book(ID INTEGER PRIMARY KEY,ISBN VARCHAR(255) PRIMARY KEY,Publisher_Name TEXT(50),Author_Name TEXT(200),Author_Address TEXT(100),year INTEGER,Title TEXT(50),Price INTEGER)')
         self.conn2.commit()
 
-        self.author.execute('CREATE TABLE IF NOT EXISTS author(Name TEXT(40),Address TEXT(100),URL TEXT(30))')
+        self.author.execute('CREATE TABLE IF NOT EXISTS author(AName TEXT(40),Address TEXT(100),URL TEXT(30))')
         self.conn3.commit()
 
     def insert(self):
